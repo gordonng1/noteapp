aed5390677703687cd7f4acc658b38135253f5c3
gordonng <gordonng@umich.edu>
2022-08-08 22:56:05 -0400

    fixed setting name and content functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <map>
 #include "Note.h"
+#include <vector>
+
 using namespace std;
+
+map<string, string> notes;
+vector<string> noteNames;
+bool quit = false;
 
 void printOptions() {
     cout << "Options Menu\n";
@@ -12,45 +19,144 @@ void printOptions() {
     cout << "6. Quit Program\n\n";
 }
 
-void promptNoteName(string &nameIn) {
-    cout << "Name of New Note: \n";
-    cin >> nameIn;
+string getStringInput() {
+    string input;
+    getline(cin, input);
+
+    while (cin.fail()) {
+        cout << "BAD" << endl;
+        cin.clear();
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // #ignore clears the rest of the input stream
+        getline(cin, input);
+    }
+    return input;
 }
 
-void promptNoteContent(string &contentIn) {
-    cout << "Content of Note: \n";
-    cin >> contentIn;
+// allows user to set the name of the note
+void setNoteName(string &nameIn) {
+    cout << "Name of note: \n";
+
+    //grabs input from user and then clears input stream
+    nameIn = getStringInput();
+
+    cout << "Note has been named: " << nameIn << endl;
+}
+
+// allows user to set the contents of the note
+void setNoteContent(string &contentIn) {
+    cout << "Enter content of note: \n";
+
+    //grabs input from user and then clears input stream
+    contentIn = getStringInput();
+
+    cout << "Your note reads: " << contentIn << endl;
+}
+
+int getIntInput() {
+    int result;
+    cin >> result; // if input cant be read, cin's failbit is set, ie., cant read from stream
+    while (cin.fail()) {  // if cin is errored right now
+        cout << "BAD" << endl;
+        cin.clear(); // #clear will unset the failbit, ie., error state unset
+        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // #ignore clears the rest of the input stream
+        cin >> result; // try to take another input
+    }
+
+    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // #ignore clears the rest of the input stream
+    return result;
+}
+// [] input stream
+
+
+// creates note
+void createNewNote() {
+    Note n;
+    string nameIn;
+    string contentIn;
+
+    setNoteName(nameIn);
+    n.setName(nameIn);
+
+    // adds note name into notes list
+    noteNames.push_back(nameIn);
+
+    setNoteContent(contentIn);
+    n.setContent(contentIn);
+
+    // inserts note into notes map
+    notes.insert(pair<string, string>(n.getName(), n.getContent()));
+}
+
+void printNoteNames() {
+    for (int i = 0; i < noteNames.size(); i++) {
+        cout << noteNames[i] << endl;
+    }
+}
+
+void viewNote() {
+    string note;
+    cout << "Which note would you like to view: \n";
+    // prints out all note names
+    printNoteNames();
+
+    cin.clear();
+    cin.ignore(256, '\n');
+
+    getline(cin, note);
+
+    if (notes.find(note) != notes.end()) {
+        cout << notes.find(note)->second << endl;
+
+        cin.clear();
+        cin.ignore(256, '\n');
+    }
+
+    else {
+        cout << "Note not found\n";
+    }
+}
+
+void deleteNote() {
+    string noteName;
+    cout << "insert list of all notes\n";
+    cout << "Which note would you like to delete: \n";
+    cin >> noteName;
+
+    if (notes.find(noteName) != notes.end()) {
+        notes.erase(noteName);
+    }
 }
 
 void takeInput() {
     int option;
     cout << "Enter Option: \n";
     cin >> option;
+    cin.clear();
+    cin.ignore(256, '\n');
 
     if (option == 1) {
-        Note n;
-        string nameIn;
-        string contentIn;
-        promptNoteName(nameIn);
-        promptNoteContent(contentIn);
-        n.setName(nameIn);
-        n.setContent(contentIn);
+        createNewNote();
     }
     else if (option == 2) {
+        viewNote();
 
     }
     else if (option == 3) {
+        deleteNote();
 
     }
     else if (option == 4) {
+        printNoteNames();
 
     }
     else if (option == 5) {
 
     }
     else if (option == 6) {
-
+        quit = true;
+        cout << "Quitting Notes App\n";
     }
+
     else {
         cout << "Invalid Option";
     }
@@ -59,17 +165,12 @@ void takeInput() {
 
 
 int main() {
-    printOptions();
-    Note n1;
+    while (!quit) {
+        printOptions();
+        takeInput();
+    }
 
-    cout << n1.getName() << endl;
-    cout << n1.getContent() << endl;
 
-    n1.setName("note 1");
-    n1.setContent("this is note 1");
-
-    cout << n1.getName() << endl;
-    cout << n1.getContent() << endl;
 
     return 0;
 }
